ec485c48c94c0d3885c30125aa93a86ff99a1657
SebastianMT1 <00161024@uca.edu.sv>
2024-05-15 21:27:06 -0600

    ejercicios completados

--- ecuacion.cpp
+++ ecuacion.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<math.h>
+#include<cmath>
 
 using namespace std;
 
@@ -16,9 +16,23 @@ int main(){
     cout<<"Ingrese el valor de c: ";
     cin>>c;
 
-    raiz = sqrt(b*b-(4*a*c));
+    if (a == 0){
+        cout<<"La respuesta a la ecuacion no es un numero real";
+    }
 
-    cout<<raiz;
+    else if (b == 0){
+        cout<<"La respuesta a la ecuacion no es un numero real";
+    }
+
+    else{
+    raiz = sqrt((pow(b,2)) - (4*a*c));
+
+    x1 = ((b*-1) + raiz)/(2*a);
+
+    x2 = ((b*-1) - raiz)/(2*a);
+    
+    cout<<"Las respuestas a la ecuacion son "<<x1<<" y "<<x2;
+    }
     
     return 0;
 }
--- estadio.cpp
+++ estadio.cpp
@@ -4,5 +4,54 @@ using namespace std;
 
 int main(){
 
+    int sector = 0;
+    int entradas = 0;
+    int total = 0;
+
+    cout<<"Ingrese el sector del estadio en el que le gustaria estar:\n";
+    cout<<"Sol general (1)\n";
+    cout<<"Sol preferente (2)\n";
+    cout<<"Sombra (3)\n";
+    cout<<"Tribuna  (4)\n";
+    cout<<"Platea (5)\n\n";
+    cin>>sector;
+
+    cout<<"Ingrese el numero de entradas: ";
+    cin>>entradas;
+    cout<<"\n";
+
+    switch (sector){
+        
+        case 1:
+        total = entradas * 3;
+        cout<<"El total a pagar de las entradas es de "<<total<<"$";
+        break;
+
+        case 2:
+        total = entradas * 5;
+        cout<<"El total a pagar de las entradas es de "<<total<<"$";
+        break;
+
+        case 3:
+        total = entradas * 8;
+        cout<<"El total a pagar de las entradas es de "<<total<<"$";
+        break;
+
+        case 4:
+        total = entradas * 15;
+        cout<<"El total a pagar de las entradas es de "<<total<<"$";
+        break;
+
+        case 5:
+        total = entradas * 20;
+        cout<<"El total a pagar de las entradas es de "<<total<<"$";
+        break;
+
+        default:
+        cout<<"Numero de sector inexistente";
+    
+        break;
+    }
+
     return 0;
 }
